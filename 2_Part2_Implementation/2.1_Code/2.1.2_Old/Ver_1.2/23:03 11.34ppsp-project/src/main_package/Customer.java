625985205f121b08f954c18059f955c9623527a5
Dave Russell <=>
2011-04-01 09:30:00 +0100

    Another final commit 01/04/2011 - 9.29

---
+++ 2_Part2_Implementation/2.1_Code/2.1.2_Old/Ver_1.2/23:03 11.34ppsp-project/src/main_package/Customer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main_package;
+
+/**
+ *
+ * @author James
+ */
+public class Customer extends Person {
+    //Initialise variables
+      private int intCustomerID;
+      private String strFirstName;
+      private String strLastName;
+      private int intAge;
+      private String strTelephone;
+     
+      public Customer (String strTheFirstname, String strTheLastname, int intTheAge, String strTheTelephone, int intTheCustomerID){
+
+          super(strTheFirstname, strTheLastname, intTheAge, strTheTelephone);
+          this.intCustomerID = intTheCustomerID;
+
+          this.strFirstName = strTheFirstname;
+          this.strLastName = strTheLastname;
+          this.intAge = intTheAge;
+          this.strTelephone = strTheTelephone;
+
+   }
+       //toString overide
+      public String toString()  {
+        String s = "Customer first name: " + strFirstName + " Customer last name: " + strLastName + " Customer age: " + intAge + " "
+                + " Customer Telephone: " + strTelephone + " Customer ID: " + intCustomerID;
+
+              return s;
+    }
+
+}
